9787b21fed7dbda62147b460f5d7bfb6adf766ce
austinugbaja <augustineugbaja97@gmail.com>
2021-09-21 06:46:59 +0000

    made changes to the Navbar component
    also made changes to the EventMail.jsx

--- src/Components/EventMail.jsx
+++ src/Components/EventMail.jsx
@@ -2,20 +2,16 @@ import React from 'react'
 import './Styles/EventMail.scss'
 import { Link } from 'react-router-dom'
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import {  faFacebookF , faLinkedin, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import fbicon from './Images/facebookicon.png'
-import igicon from './Images/instagramicon.png'
-import linkdlnicon from './Images/linkedinicon.png'
-import twittricon from './Images/twittericon.png'
 
  const EventMail = () => {
     return (
         <div className='EventMail-section'>
-          
                <div className="row">
                     <div className='title1 col-lg-6'>
+                        <div className="container">
                         <h1 className='first'>
                         Join Mailing List
                         </h1>
@@ -24,9 +20,7 @@ import twittricon from './Images/twittericon.png'
                         address, confirm you are happy to receive our emails and then select  ‘Subscribe’.
                         </h2>
 
-                        
-                      
-                        <div class="name1">
+                         <div class="name1">
                             <label class="visually-hidden" for="autoSizingInput">First Name</label>
                             <input type="text" placeholder="First Name"
                             class="forms text-start" id="autoSizingInput" >
@@ -41,6 +35,8 @@ import twittricon from './Images/twittericon.png'
                         <Link to='/'>
                             <button type="button" class=" submits">Submit</button>
                         </Link>
+                        </div>
+                        
                     </div>
                     <div className=' col-lg-6 title2'>
                     <div className='event'>
@@ -105,8 +101,9 @@ import twittricon from './Images/twittericon.png'
                         </div>
                     </div>
                 </div>
+                <div className="container">
                 <div class="row">
-                    <div class="col-lg-6 text-start contact-us">
+                    <div class="col-md-4 text-start contact-us">
                     <div className="location">
                         Rivers State ICT Department, opposite the Pleasure Park,
                         Aba Road, Port Harcourt
@@ -117,21 +114,24 @@ import twittricon from './Images/twittericon.png'
                     </div>
                     <div> <FontAwesomeIcon icon={faPhoneAlt} /> 09030003185, 09030003180</div>
                     <div className="icons">
-                        <img src={fbicon} alt="facebook icon" className="fb" />
-                        <img src={linkdlnicon} alt="linkedIn icon" className="linkln" />
-                        <img src={twittricon} alt="twitter icon" className="twitr" />
-                        <img src={igicon} alt="instagram icon" className="ig" />
+                        <FontAwesomeIcon icon={faFacebookF} className="footer-social-icons" />
+                        <FontAwesomeIcon icon={faLinkedin} className="footer-social-icons" />
+                        <FontAwesomeIcon icon={faTwitter} className="footer-social-icons" />
+                        <FontAwesomeIcon icon={faInstagram} className="footer-social-icons" />
                     </div>
                     </div>
                     </div>
-                    <div class="col-lg-6 text-end copyright">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-2"></div>
+                    <div class="col-md-4 text-end copyright">
                     <div className="copy-right">
-                    Copyright 2021 Tech Creek, All Right Reserved
+                    &#169; 2021 Tech Creek, All Right Reserved
                     </div>
                     <div className="policy">
                     Privacy Policy  | Terms Of Use
                     </div>
                     </div>
+                </div>
                 </div>
         </div>
        
